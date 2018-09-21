ecff89001da9d7c8deaf129ed2a24232b8f60acc
Crypter <crypter@mail.com>
2018-09-21 22:50:36 +0200

    esp32 port with butchered code

--- ESP-RFID.ino
+++ ESP-RFID.ino
@@ -1,18 +1,14 @@
-#include <ESP8266WiFi.h>
-#include <ESP8266WiFiMulti.h>
-#include <ESP8266WebServer.h>
+#include <WiFi.h>
+#include <WebServer.h>
 #include <FS.h>
+#include <SPIFFS.h>
 #include <ArduinoJson.h>
 #include <EEPROM.h>
+#include <rom/rtc.h>
 
-#include <ESP8266mDNS.h>
-#include <WiFiUdp.h>
-#include <ArduinoOTA.h>
 
-ADC_MODE(ADC_VCC);
-
-#define RF_PIN D1
-#define ACTIVITY_LED D4
+#define RF_PIN 19
+#define ACTIVITY_LED BUILTIN_LED
 
 static char DEBUGMODE=0;
 //helpers for debugging timings
@@ -77,16 +73,10 @@ void prepare_data(uint32_t ID, uint32_t VENDOR){
   */
 }
 
-ESP8266WiFiMulti WiFiMulti;
-ESP8266WebServer server(80);
+WebServer server(80);
 
 void handleRoot() {
   webResponse = "<!DOCTYPE html><meta charset=UTF-8><title>RFID Ugly panel</title><script>function httpGetAsync(e,t){var n=new XMLHttpRequest;n.onreadystatechange=function(){4==n.readyState&&200==n.status&&t&&t(n.responseText)},n.open(\"GET\",e,!0),n.send(null)}window.onload=function(){httpGetAsync(\"/api\",setup)};function setup(e){e=JSON.parse(e);for(var t=document.getElementById(\"available\"),n=0;n<e.rfid.length;n++){var o=document.createElement(\"option\");o.text=e.rfid[n].name+\" (\"+e.rfid[n].vendor+\"|\"+e.rfid[n].uid+\")\",o.value=e.rfid[n].id,t.options.add(o,o.value)}for(n=0;n<t.length;n++)if(t.options[n].value==e.active_id){t.options.selectedIndex=n;break}}function update(){var e=document.getElementById(\"available\");return console.log(e.options[e.options.selectedIndex].value),httpGetAsync(\"/api?rfid=\"+e.options[e.options.selectedIndex].value),!1}</script><style>button,input,select{display:block;margin:1px}button,input{min-width:200px}body{background-color:#eee}</style><h1>Welcome to<br><strike>Admin</strike>Ugly panel</h1><form action=/api onsubmit=\"return update()\"><select id=available><option value=0>Disabled</select><input type=submit value=Update></form><button id=wifi onclick='location.href=\"/wifi\"'>WiFi Setup</button> <button id=rfid onclick='location.href=\"/rfid\"'>RFID Setup</button>";
-  server.send(200, "text/html", webResponse.c_str());
-}
-
-void handleWifi() {
-  webResponse = "<!DOCTYPE html><meta charset=UTF-8><title>RFID Ugly panel</title><script>function httpGetAsync(e,t){var n=new XMLHttpRequest;n.onreadystatechange=function(){4==n.readyState&&200==n.status&&t&&t(n.responseText)},n.open(\"GET\",e,!0),n.send(null)}window.onload=function(){httpGetAsync(\"/api\",setup)};function reload(){location.reload()}function setup(e){e=JSON.parse(e);for(var t=document.getElementById(\"wifis\"),n=0;n<e.wifi.length;n++){var o=document.createElement(\"option\");o.text=e.wifi[n].ssid+\" (\"+e.wifi[n].pass.length+\")\",o.value=e.wifi[n].id,t.options.add(o,o.value)}}function remove(){var e=document.getElementById(\"wifis\");return console.log(e.options[e.options.selectedIndex].value),httpGetAsync(\"/api?remove=wifi&id=\"+e.options[e.options.selectedIndex].value),e.options.remove(e.options.selectedIndex),!1}function add(){return console.log(\"/api?add=wifi&ssid=\"+document.getElementById(\"ssid\").value+\"&pass=\"+document.getElementById(\"pass\").value),httpGetAsync(\"/api?add=wifi&ssid=\"+document.getElementById(\"ssid\").value+\"&pass=\"+document.getElementById(\"pass\").value,reload),!1}</script><style>button,input,select{display:block;margin:1px}button,input{min-width:200px}body{background-color:#eee}</style><form action=/api onsubmit=\"return remove()\"><select id=wifis></select><input type=submit value=Delete></form><form action=/api onsubmit=\"return add()\"><span>SSID: </span><input id=ssid> <span>PASS: </span><input type=password id=pass> <input type=submit value=Add></form><button id=wifi onclick='location.href=\"/\"'>Home</button> <button id=rfid onclick='location.href=\"/rfid\"'>RFID Setup</button>";
   server.send(200, "text/html", webResponse.c_str());
 }
 
@@ -168,7 +158,7 @@ void handleApi() {
         JsonArray &rfids = config["rfid"];
         int i=0, idmax=0;
         for (i=0; i< rfids.size(); i++){
-          if (rfids[i]["uid"] == server.arg("uid").toInt()) break;
+          if (rfids[i]["uid"] == server.arg("uid").toInt() && rfids[i]["vendor"]  ==server.arg("vendor").toInt()) break;
           idmax = (rfids[i]["id"] < idmax)? idmax:rfids[i]["id"];
         }
         if (i==rfids.size()){
@@ -220,7 +210,6 @@ void handleApi() {
   }
 
   webResponse = "";
-  config["battery"] = ESP.getVcc()/901.515;
   config.prettyPrintTo(webResponse);
   configFile.close();
   server.sendHeader("Access-Control-Allow-Origin", "*");
@@ -232,7 +221,7 @@ void handleReboot() {
   server.send(200, "text/html", webResponse.c_str());
   delay(5000);
   SPIFFS.end();
-  ESP.reset();
+  ESP.restart();
   delay(1000);
 }
 
@@ -242,24 +231,33 @@ void handleFormat() {
   delay(5000);
   SPIFFS.end();
   SPIFFS.format();
-  ESP.reset();
+  ESP.restart();
   delay(1000);
 }
+
+void httpLoop(void *pvParameters) {
+  while (1){
+  server.handleClient();
+  delay(30);
+  }
+}
+
 
 void setup() {
   EEPROM.begin(1);
   state = EEPROM.read(0) & 1;
 
-  if (ESP.getResetReason() == "Deep-Sleep Wake" | ESP.getResetReason() == "External System") {
+  if (rtc_get_reset_reason(0)==1) {
     state = !state;
     EEPROM.write(0, state);
     EEPROM.commit();
+//    delay(5);
   }
 
-  if (!state) {
-    ESP.deepSleep(0);
-  }
   EEPROM.end();
+  if (!state) {
+    ESP.deepSleep(1000000);
+  }
 
 
   pinMode(RF_PIN, INPUT);
@@ -269,27 +267,23 @@ void setup() {
 //  digitalWrite (1, LOW);
 
   WiFi.disconnect(true); //true = WiFi.mode(WIFI_OFF);
-  delay(50);
+  delay(5);
 
   if (DEBUGMODE) Serial.begin(115200);
 
 
 //  WiFi.persistent(false); //Do NOT write to flash;
-  WiFi.mode(WIFI_STA);
+  WiFi.mode(WIFI_AP);
+  WiFi.softAP("RFID");
 
-  SPIFFS.begin();
+  SPIFFS.begin(1);
   if (SPIFFS.exists("/rfid.conf")){
     File configFile = SPIFFS.open("/rfid.conf", "r");
     DynamicJsonBuffer jsonBuffer;
     JsonObject &config = jsonBuffer.parseObject(configFile);
 
-    DEBUGMODE = config["debug"];
+//    DEBUGMODE = config["debug"];
     active_id = config["active_id"];
-
-    network_count=config["wifi"].size();
-    for (int i=0; i<config["wifi"].size(); i++) {
-      WiFiMulti.addAP(config["wifi"][i]["ssid"], config["wifi"][i]["pass"]);
-    }
 
     for (int i=0; active_id && i<config["rfid"].size(); i++) {
       if (active_id == config["rfid"][i]["id"]) {
@@ -304,51 +298,21 @@ void setup() {
     File configFile = SPIFFS.open("/rfid.conf", "w");
     DynamicJsonBuffer jsonBuffer;
     JsonObject &config = jsonBuffer.createObject();
-    config["debug"] = DEBUGMODE;
+//    config["debug"] = DEBUGMODE;
     config["active_id"] = 0;
     JsonArray &rfconfig = config.createNestedArray("rfid");
-    JsonArray &wificonfig = config.createNestedArray("wifi");
     config.printTo(configFile);
     configFile.close();
   }
 
   server.on("/", handleRoot);
   server.on("/api", handleApi);
-  server.on("/wifi", handleWifi);
   server.on("/rfid", handleRfid);
   server.on("/reboot", handleReboot);
   server.on("/format", handleFormat);
+  server.begin();
 
-  ArduinoOTA.onStart([]() {
-    String type;
-    if (ArduinoOTA.getCommand() == U_FLASH)
-      type = "sketch";
-    else // U_SPIFFS
-      type = "filesystem";
-
-    // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
-    if (DEBUGMODE) Serial.println("Start updating " + type);
-  });
-  ArduinoOTA.onEnd([]() {
-    if (DEBUGMODE) Serial.println("\nEnd");
-  });
-  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    if (DEBUGMODE) Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-  });
-  ArduinoOTA.onError([](ota_error_t error) {
-    if (DEBUGMODE){
-      Serial.printf("Error[%u]: ", error);
-      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-      else if (error == OTA_END_ERROR) Serial.println("End Failed");
-    }
-  });
-
-//  MDNS.begin("rfid");
-//  MDNS.addService("http", "tcp", 80);
-//  MDNS.update();
+  xTaskCreatePinnedToCore(httpLoop, "HTTP", 16384 , NULL, 0, NULL, 0);
 
   prepare_data(ID_active, vendor_active);
 
@@ -358,52 +322,25 @@ void setup() {
 
 void loop() {
 
-  if (delayed_setup) {
-      if(WiFiMulti.run() == WL_CONNECTED){
-        server.begin();
-        ArduinoOTA.begin();
-        delayed_setup=0;
-        if (DEBUGMODE) Serial.printf("WiFi connected, IP: %s\r\n", WiFi.localIP().toString().c_str());
-      }
-      else if (millis()>15000 || !network_count){
-        if (DEBUGMODE) Serial.println("WiFi failed, Hotspot up...");
-        WiFi.scanDelete();
-        WiFi.disconnect();
-        WiFi.mode(WIFI_AP);
-        WiFi.softAP("RFID");
-        server.begin();
-        ArduinoOTA.begin();
-        delayed_setup=0;
-      }
-  }
-  else {
-    ArduinoOTA.handle();
-    server.handleClient();
-  }
-
-  digitalWrite (ACTIVITY_LED, LOW);
-
-  delay(10);
-  int i=0, j=0;
+//  delay(10);
+  int j=0;
   //Manchester
   if (active_id){
-    for (i=0; i<15; i++){
+      digitalWrite (ACTIVITY_LED, LOW);
       for (j=0; j<64; j++){
-
-//        data[j]? pinMode(RF_PIN, OUTPUT):pinMode(RF_PIN, INPUT);
+        data[j]? pinMode(RF_PIN, OUTPUT):pinMode(RF_PIN, INPUT);
 //        data[j]? (GPE |= 0b00000100):(GPE &= ~0b00000100);
-        data[j]? (GPE |= (1<<RF_PIN)):(GPE &= ~(1<<RF_PIN));
+//        data[j]? (GPE |= (1<<RF_PIN)):(GPE &= ~(1<<RF_PIN));
         delayMicroseconds(255);
-        for (int k=0; k<14; k++) asm_nop(); //fine tuning
+//        for (int k=0; k<14; k++) asm_nop(); //fine tuning
 
-//        data[j]? pinMode(RF_PIN, INPUT):pinMode(RF_PIN, OUTPUT);
+        data[j]? pinMode(RF_PIN, INPUT):pinMode(RF_PIN, OUTPUT);
 //        data[j]? (GPE &= ~0b00000100):(GPE |= 0b00000100);
-        data[j]? (GPE &= ~(1<<RF_PIN)):(GPE |= (1<<RF_PIN));
+//        data[j]? (GPE &= ~(1<<RF_PIN)):(GPE |= (1<<RF_PIN));
         delayMicroseconds(255);
-        for (int k=0; k<13; k++) asm_nop(); //fine tuning
+//        for (int k=0; k<13; k++) asm_nop(); //fine tuning
 
       }
-    }
     digitalWrite (ACTIVITY_LED, HIGH);
   }
 }
